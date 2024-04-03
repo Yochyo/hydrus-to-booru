7a7544879a60a3cdbfa5b639d6db5ef41e4bfc69
yochyo <herodragon25@gmail.com>
2024-04-03 14:16:26 +0200

    temp

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,9 @@
-console.log('test');
+import * as fs from 'fs';
+import sharp from 'sharp';
+
+const
+
+void (async  () => {
+  const a = fs.readFileSync('files/sample_9e20ea741ddcff5d2962e446f9733c2f9cc74b7f.jpg')
+  await sharp(a).resize({height: 200}).toFile('temp.png')
+})()
