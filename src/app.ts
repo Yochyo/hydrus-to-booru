e8615d07da9c1d918374924504774928f11cb0ca
yochyo <herodragon25@gmail.com>
2024-05-05 19:00:09 +0200

    temp

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,11 @@
 import { SzurubooruApi } from '../../szurubooru-api/src/szurubooru-api';
 import { HydrusApi } from '../../hydrus-api/src/hydrus-api';
 import { FileMetadataResponse } from '../../hydrus-api/src/api/get-files/types';
+import sharp from 'sharp';
 
 type Tag = {name: string, category: string}
+
+const INCLUDE_LEVEL = 1
 
 const api = new HydrusApi({host: 'http://localhost:45869/', accessKey: '1961a1878a2e8f8899ac992805b8daa415fbc514f412966f26e7c59318e9f876'})
 const szuru = new SzurubooruApi({host: 'http://localhost:8080/api/', username: 'admin', password: 'habdichlieb'})
@@ -12,8 +15,10 @@ const getRelevantTags = (meta: FileMetadataResponse): Record<number, Tag[]> => {
     const tags = Object.values(input).flat()
     const map = new Map<string, Set<string>>()
     tags.forEach(tag => {
-      if(map.has(tag.name)) map.get(tag.name)!.add(tag.category)
-      else map.set(tag.name, new Set([tag.category]))
+      if(tag.category != 'sherrybooru') {
+        if(map.has(tag.name)) map.get(tag.name)!.add(tag.category)
+        else map.set(tag.name, new Set([tag.category]))
+      }
     })
     const tagNamesToRename: string[] = []
     for (let entry of map.entries()) {
@@ -24,7 +29,7 @@ const getRelevantTags = (meta: FileMetadataResponse): Record<number, Tag[]> => {
     return Object.fromEntries(Object.entries(input).map(entry => [entry[0], entry[1].map(tag => tagNamesToRename.includes(tag.name) ? ({name: `${tag.name}_(${tag.category})`, category: tag.category}) : tag)]))
   }
 
-  const allowedNamespaces = ['default', 'creator', 'meta', 'character', 'series']
+  const allowedNamespaces = ['default', 'creator', 'meta', 'character', 'series', 'sherrybooru']
 
   const tags = meta.metadata.map(meta => meta.tags)
   const myTags = tags.map(tagObj => Object.fromEntries(Object.entries(tagObj).filter(entry => entry[0] == '6c6f63616c2074616773'))) // only use "my tags"
@@ -36,23 +41,6 @@ const getRelevantTags = (meta: FileMetadataResponse): Record<number, Tag[]> => {
 }
 
 const createTags = async (szuru: SzurubooruApi, tags: Tag[]) => {
-  const filterDuplicateTags = () => {
-    const map = new Map<string, Set<string>>()
-    tags.forEach(tag => {
-      if(map.has(tag.name)) map.get(tag.name)!.add(tag.category)
-      else map.set(tag.name, new Set(tag.category))
-    })
-    const uniqueTags: Tag[] = []
-    for (let entry of map.entries()) {
-      const size = entry[1].size
-      if(size > 1) {
-        entry[1].forEach(category => uniqueTags.push({name: `${entry[0]}_(${category})`, category}))
-      }
-      if(size == 1) uniqueTags.push({name: entry[0], category: Array.from(entry[1])[0]})
-      else console.warn(`tag ${entry[0]} does not have a category!?!?`)
-    }
-    return uniqueTags
-  }
   const createNS = async () => {
     const ns: Record<string, {color: string, order: number}> = {
       creator: {color: '#a40202', order: 1},
@@ -66,44 +54,51 @@ const createTags = async (szuru: SzurubooruApi, tags: Tag[]) => {
     await Promise.all(Object.entries(ns).map(entry => szuru.createCategory({payload: {name: entry[0], color: entry[1].color, order: entry[1].order}}).catch(it => it)))
   }
   const createTags = async () => {
-    await Promise.all(tags.map(tag => szuru.createTag({payload: {category: tag.category, names: [tag.name]}}).catch(it => it)))
+    // todo stringify does not respect field order, use something else
+    const uniqueTags = Array.from(new Set(tags.map((o) => JSON.stringify(o)))).map((s) => JSON.parse(s))
+
+    await Promise.all(tags.map(tag => szuru.createTag({payload: {category: tag.category, names: [tag.name]}}).catch((it: any) => it)))
   }
   await createNS()
-
+  await createTags()
 }
 
-const uploadImage = async (hash: string) => {
-  const getTags = (meta: FileMetadataResponse) => {
-    const services = meta.metadata[0].tags
-    const filtered = Object.fromEntries(Object.entries(services).filter(entry => entry[0] == '6c6f63616c2074616773')) // only use "my tags"
-    const tags = Object.values(filtered).flatMap(it => it.storage_tags['0']).filter(it => it) as string[]
-
-    const allowedNamespaces = ['default', 'creator', 'meta', 'character', 'series']
-    return tags.map(tag => ({category: tag.includes(':') ? tag.substring(0, tag.indexOf(':')) : 'default', name: tag.includes(':') ? tag.substring(tag.indexOf(':') + 1) : tag})).filter(tag => allowedNamespaces.includes(tag.category))
+const uploadImage = async (meta: FileMetadataResponse['metadata'][0], tags: Tag[]) => {
+  const calcDimensions = () => {
+    const regex = /^res(-(\d+))?(=(\d*)x(\d*))?$/
+    const {width, height}: {width?: number, height?: number} = {}
+    const resolutionTags = tags.filter(tag => tag.category == 'sherrybooru' && regex.test(tag.name))
+    const resolutions: {level: number, width?: number, height?: number} = resolutionTags.map(tag => tag.name.match(regex)).map(it => ({level: +(it![2] ?? 1), width: it![4] == undefined ? undefined : +(it![4]), height: it![5] == '' ? undefined : +(it![5])}))
   }
-  const createTags = async (tags: {category: string, name: string}[]) => {
-    await Promise.all(tags.map(tag => szuru.createTag({payload: {category: tag.category, names: [tag.name.replaceAll(' ', '_')]}}).catch(it => console.log(`${tag.name} already exists or something like that`))))
-  }
-
-
   const uploadFile = async (meta: FileMetadataResponse, tags: {category: string, name: string}[]) => {
+
     const file = await api.getFile({hash})
     // todo rating
     await szuru.createPosts({upload: {content: file}, payload:{ tags: tags.map(it => it.name), safety: tags.find(tag => tag.category == 'rating')?.name ?? 'safe', source: meta.metadata[0].known_urls.join('\n')}})
   }
-  const meta = await api.getFileMetadata({hash})
-  const tags = getTags(meta).map(tag => ({...tag, name: tag.name.replaceAll(" ", "_")}))
-  await createTags(tags)
   await uploadFile(meta, tags)
   console.log(tags);
 }
 
 void (async  () => {
-  const metas = await api.getFileMetadata({file_ids: (await api.searchFiles({ tags: ['sherrybooru:*'] })).file_ids})
-  const tags = getRelevantTags(metas)
+  const [metas, tags] = await (async () => {
+    const fileIds = (await api.searchFiles({ tags: ['sherrybooru:*'] })).file_ids
+    const metas = await api.getFileMetadata({file_ids: fileIds})
+    let tags = getRelevantTags(metas)
+    // exclude images with sherrybooru:exclude and !sherrybooru:include include level too low
+    let excludePostsWithIds = Object.keys(tags).map(fileId => +fileId)
+    excludePostsWithIds = excludePostsWithIds.filter(fileId => !tags[fileId].some(tag => !(tag.category == 'sherrybooru' && tag.name == 'exclude')))
+    excludePostsWithIds = excludePostsWithIds.filter(fileId => !tags[fileId].some(tag => (tag.category == 'sherrybooru' && /^include(-\d+)?$/.test(tag.name))))
+    excludePostsWithIds = excludePostsWithIds.filter(fileId => tags[fileId].some(tag => (tag.category == 'sherrybooru' && +(tag.name.match(/^include(-(\d+))?$/)![2] ?? 1) <= INCLUDE_LEVEL)))
+
+    metas.metadata = metas.metadata.filter(meta => !excludePostsWithIds.includes(meta.file_id))
+    tags = Object.fromEntries(Object.entries(tags).filter(entry => excludePostsWithIds.includes(+entry[0])))
+    return [metas, tags]
+  })()
+  // create tags and their categories so that posts can use them (or else every tag a post uses has the default category)
+  await createTags(szuru, Object.values(tags).flat().filter(it => it.category != 'sherrybooru'))
   return
 
-  // await createNS(szuru)
   await uploadImage('cd4c24717d6d8aae98d803506248a053224af7f3dabf9bf0ffb8d1c2f256f4e8')
   return
   // await Promise.all(mappedTags.filter(it => it.category).map(tag => szuru.createCategory({payload: ({name: tag.category, color: 'white', order: 1})})))
@@ -113,3 +108,12 @@ void (async  () => {
 
 
 
+
+const findTag = (meta: FileMetadataResponse['metadata'][0], str: string): Tag? => {
+  const tags = meta.tags
+  const myTags = Object.fromEntries(Object.entries(tags).filter(entry => entry[0] == '6c6f63616c2074616773'))
+  const myTagsStorage = Object.values(myTags).flatMap(it => it.storage_tags['0']).filter(it => it) as string[]
+  const res = myTagsStorage.map(tags => tags.map(tag => ({category: tag.includes(':') ? tag.substring(0, tag.indexOf(':')) : 'default', name: (tag.includes(':') ? tag.substring(tag.indexOf(':') + 1) : tag).replaceAll(' ', "_")})).filter(tag => allowedNamespaces.includes(tag.category)))
+  const relevantTags =  Object.fromEntries(meta.metadata.map((it, i) => [it.file_id, res[i]]))
+
+}
